ed3b209a8c90ced6c3f2fa2b281de45f88afe5a1
Rjmnhr <88718714+Rjmnhr@users.noreply.github.com>
2023-07-26 15:12:08 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
 import "./App.css";
 import AppRoute from "./app-route";
 import { AppContextProvider } from "./context/app-context";
-// import { DndProvider } from "react-dnd";
+import { DndProvider } from "react-dnd";
 // import { MultiBackend } from "react-dnd-multi-backend";
-// import { HTML5Backend } from "react-dnd-html5-backend";
+import { HTML5Backend } from "react-dnd-html5-backend";
 // import { TouchBackend } from "react-dnd-touch-backend";
 
 function App() {
-  //   const HTML5toTouch = {
-  //     backends: [
-  //       {
-  //         backend: HTML5Backend,
-  //       },
-  //       {
-  //         backend: TouchBackend,
-  //         options: { enableMouseEvents: true, enableTouchEvents: true },
-  //         preview: true,
-  //       },
-  //     ],
-  //   };
+  // const HTML5toTouch = {
+  //   backends: [
+  //     {
+  //       backend: HTML5Backend,
+  //     },
+  //     {
+  //       backend: TouchBackend,
+  //       options: { enableMouseEvents: true, enableTouchEvents: true },
+  //       preview: true,
+  //     },
+  //   ],
+  // };
   return (
     <div className="App">
       {/* <DndProvider backend={MultiBackend} options={HTML5toTouch}> */}
-      <AppContextProvider>
-        <AppRoute />
-      </AppContextProvider>
-      {/* </DndProvider> */}
+      <DndProvider backend={HTML5Backend}>
+        <AppContextProvider>
+          <AppRoute />
+        </AppContextProvider>
+      </DndProvider>
     </div>
   );
 }
