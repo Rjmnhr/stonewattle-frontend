41f0d717b5d8509e93d950564fe4117bd5f1e341
rjmnhr1 <renjith.cm@refactor.academy>
2023-09-14 23:41:13 +0530

    update

--- src/components/axios.js
+++ src/components/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const AxiosInstance = axios.create({
-  // baseURL: "https://backend.2ndstorey.com", // Replace with your API base URL
-  baseURL: "http://localhost:8002",
+  baseURL: "https://backend.2ndstorey.com", // Replace with your API base URL
+  // baseURL: "http://localhost:8002",
   timeout: 5000, // Request timeout in milliseconds
 });
 
